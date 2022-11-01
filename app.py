e0faeacbe7b8cf6ecfc191311c442da4c6e7b753
ptg0811 <ptg0811@gmail.com>
2022-11-01 16:08:13 +0900

    커밋시도

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask
+app = Flask(__name__)
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://travel.naver.com/overseas?seasonTab=theme&type=SC&theme=%EC%8B%A0%ED%98%BC&page=1',headers=headers)
+@app.route('/')
+def home():
+   return 'This is Home!'
 
-soup = BeautifulSoup(data.text, 'html.parser')
+@app.route('/mypage')
+def mypage():
+   return 'This is Mypage!'
 
-names = soup.select('#OverseasHome > div.season_Seasonals__3IGq4 > ul > li')
-
-for name in names:
-    a = name.select_one('div > div.item_keywords__ZF5rC > a')
-
-print(a)
-#OverseasHome > div.season_Seasonals__3IGq4 > ul > li:nth-child(1) > div > a.item_anchor___k2hT > div.head > span
-
-#OverseasHome > div.season_Seasonals__3IGq4 > ul > li:nth-child(1) > div > div.item_keywords__ZF5rC > a:nth-child(1)
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5001,debug=True)
